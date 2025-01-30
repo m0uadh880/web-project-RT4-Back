d520408e3e33731b5477aa985f490fd6882ed67d
Mouadh <ounissimouadh45@gmail.com>
2025-01-30 11:44:45 +0100

    updated db url

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://localhost:27017/moviesHUB")],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_CLUSTER}.lebne.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService]
 })
