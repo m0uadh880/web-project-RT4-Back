780ed2bf1af2e9482b605fe4a474b17061c5baaf
Mouadh <ounissimouadh45@gmail.com>
2025-01-30 11:50:15 +0100

    added cors configuration

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const corsOptions = {
+    origin: ['http://localhost:4201', 'http://localhost:4200'],
+    optionsSuccessStatus: 200
+    }
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
 
+  app.enableCors(corsOptions);
   await app.listen(3000);
 }
 bootstrap();
