0153abd515419bc526abbf5deac4d6a7adf2cf60
Mouadh <ounissimouadh45@gmail.com>
2025-02-01 13:17:06 +0100

    update user service

--- src/authentication/user.service.ts
+++ src/authentication/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { BaseService } from "../generics/services/base.service";
 import { User } from "src/Models/user.model";
+import { SoftDeleteModel } from "soft-delete-plugin-mongoose";
+import { InjectModel } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
 
 @Injectable()
 export class UserService extends BaseService<User> {
+  constructor(@InjectModel(User.name) model: SoftDeleteModel<User & Document>) {
+    super(model);
+  }
   async findByEmail(email: string): Promise<User> {
     const user = await this.model.findOne({ email }).exec();
     if (!user) throw new NotFoundException("User not found By email");
